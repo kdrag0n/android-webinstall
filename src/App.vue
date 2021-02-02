a3b2958ee68afdde869f296e3722db707e306654
Danny Lin <danny@kdrag0n.dev>
2021-02-01 19:18:16 -0800

    Update layout using 3-column wireframe

--- src/App.vue
+++ src/App.vue
@@ -17,35 +17,15 @@
         <v-main class="grey lighten-3">
             <v-container>
                 <v-row>
-                    <v-col cols="2">
-                        <v-sheet rounded="lg">
-                            <v-list color="transparent">
-                                <v-list-item v-for="n in 5" :key="n" link>
-                                    <v-list-item-content>
-                                        <v-list-item-title>
-                                            List Item {{ n }}
-                                        </v-list-item-title>
-                                    </v-list-item-content>
-                                </v-list-item>
+                    <v-col cols="12" sm="2"> </v-col>
 
-                                <v-divider class="my-2"></v-divider>
-
-                                <v-list-item link color="grey lighten-4">
-                                    <v-list-item-content>
-                                        <v-list-item-title>
-                                            Refresh
-                                        </v-list-item-title>
-                                    </v-list-item-content>
-                                </v-list-item>
-                            </v-list>
-                        </v-sheet>
-                    </v-col>
-
-                    <v-col>
+                    <v-col cols="12" sm="8">
                         <v-sheet min-height="70vh" rounded="lg">
                             <!--  -->
                         </v-sheet>
                     </v-col>
+
+                    <v-col cols="12" sm="2"> </v-col>
                 </v-row>
             </v-container>
         </v-main>
@@ -59,7 +39,7 @@ export default {
     components: {},
 
     data: () => ({
-        links: ["Dashboard", "Messages", "Profile", "Updates"],
+        links: ["Home", "Install"],
     }),
 };
 </script>
