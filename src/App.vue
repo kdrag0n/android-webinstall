378447073fd21b7faf1b2a2edc6d07edee5529c7
Danny Lin <danny@kdrag0n.dev>
2021-02-01 19:09:56 -0800

    Use constrained wireframe

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,77 @@
 <template>
-    <v-app>
-        <v-app-bar app color="primary" dark>
-            <div class="d-flex align-center">
-                <v-img
-                    alt="Vuetify Logo"
-                    class="shrink mr-2"
-                    contain
-                    src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-                    transition="scale-transition"
-                    width="40"
-                />
+    <v-app id="inspire">
+        <v-app-bar app color="white" flat>
+            <v-container class="py-0 fill-height">
+                <v-avatar
+                    class="mr-10"
+                    color="grey darken-1"
+                    size="32"
+                ></v-avatar>
 
-                <v-img
-                    alt="Vuetify Name"
-                    class="shrink mt-1 hidden-sm-and-down"
-                    contain
-                    min-width="100"
-                    src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-                    width="100"
-                />
-            </div>
+                <v-btn v-for="link in links" :key="link" text>
+                    {{ link }}
+                </v-btn>
 
-            <v-spacer></v-spacer>
+                <v-spacer></v-spacer>
 
-            <v-btn
-                href="https://github.com/vuetifyjs/vuetify/releases/latest"
-                target="_blank"
-                text
-            >
-                <span class="mr-2">Latest Release</span>
-                <v-icon>mdi-open-in-new</v-icon>
-            </v-btn>
+                <v-responsive max-width="260">
+                    <v-text-field
+                        dense
+                        flat
+                        hide-details
+                        rounded
+                        solo-inverted
+                    ></v-text-field>
+                </v-responsive>
+            </v-container>
         </v-app-bar>
 
-        <v-main>
-            <HelloWorld />
+        <v-main class="grey lighten-3">
+            <v-container>
+                <v-row>
+                    <v-col cols="2">
+                        <v-sheet rounded="lg">
+                            <v-list color="transparent">
+                                <v-list-item v-for="n in 5" :key="n" link>
+                                    <v-list-item-content>
+                                        <v-list-item-title>
+                                            List Item {{ n }}
+                                        </v-list-item-title>
+                                    </v-list-item-content>
+                                </v-list-item>
+
+                                <v-divider class="my-2"></v-divider>
+
+                                <v-list-item link color="grey lighten-4">
+                                    <v-list-item-content>
+                                        <v-list-item-title>
+                                            Refresh
+                                        </v-list-item-title>
+                                    </v-list-item-content>
+                                </v-list-item>
+                            </v-list>
+                        </v-sheet>
+                    </v-col>
+
+                    <v-col>
+                        <v-sheet min-height="70vh" rounded="lg">
+                            <!--  -->
+                        </v-sheet>
+                    </v-col>
+                </v-row>
+            </v-container>
         </v-main>
     </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
 export default {
     name: "App",
 
-    components: {
-        HelloWorld,
-    },
+    components: {},
 
     data: () => ({
-        //
+        links: ["Dashboard", "Messages", "Profile", "Updates"],
     }),
 };
 </script>
