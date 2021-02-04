c3287ffa69ed9c77db8d72fb141ef61655dc08be
Danny Lin <danny@kdrag0n.dev>
2021-02-04 09:15:27 -0800

    Refine install type UI

--- src/components/InstallTypeStep.vue
+++ src/components/InstallTypeStep.vue
@@ -16,54 +16,60 @@
                     outlined
                     max-width="16rem"
                     class="ma-4 d-flex flex-column justify-space-between"
+                    ripple
+                    :color="
+                        $root.$data.installType === 'clean'
+                            ? 'grey lighten-2'
+                            : null
+                    "
+                    @click="$root.$data.installType = 'clean'"
                 >
                     <div>
-                        <v-card-title>Clean install</v-card-title>
+                        <v-card-title class="mt-n2">
+                            <v-icon
+                                class="pr-2 py-2"
+                                color="rgba(0, 0, 0, 0.87)"
+                                >mdi-cellphone-erase</v-icon
+                            >
+                            Clean install</v-card-title
+                        >
                         <v-card-subtitle
                             >Switch from the stock OS or another ROM to
                             {{ $root.$data.OS_NAME }}.
-                            <strong class="red--text text--darken-3"
+                            <strong class="red--text text--darken-2"
                                 >All data on your device will be lost.</strong
                             >
                         </v-card-subtitle>
                     </div>
-
-                    <v-card-actions
-                        class="d-flex justify-space-between flex-row-reverse"
-                    >
-                        <v-btn
-                            outlined
-                            text
-                            @click="$root.$data.installType = 'clean'"
-                            >Select</v-btn
-                        >
-                    </v-card-actions>
                 </v-card>
 
                 <v-card
                     outlined
                     max-width="16rem"
                     class="ma-4 d-flex flex-column justify-space-between"
+                    ripple
+                    :color="
+                        $root.$data.installType === 'update'
+                            ? 'grey lighten-2'
+                            : null
+                    "
+                    @click="$root.$data.installType = 'update'"
                 >
                     <div>
-                        <v-card-title>Update</v-card-title>
+                        <v-card-title class="mt-n2">
+                            <v-icon
+                                class="pr-2 py-2"
+                                color="rgba(0, 0, 0, 0.87)"
+                                >mdi-update</v-icon
+                            >
+                            Update</v-card-title
+                        >
                         <v-card-subtitle
                             >Update from an older version of
                             {{ $root.$data.OS_NAME }}. Your data won’t be
                             affected.
                         </v-card-subtitle>
                     </div>
-
-                    <v-card-actions
-                        class="d-flex justify-space-between flex-row-reverse"
-                    >
-                        <v-btn
-                            outlined
-                            text
-                            @click="$root.$data.installType = 'update'"
-                            >Select</v-btn
-                        >
-                    </v-card-actions>
                 </v-card>
             </div>
         </div>
@@ -79,6 +85,12 @@
         </div>
     </v-container>
 </template>
+
+<style>
+.theme--light.v-sheet--outlined.v-card--selected {
+    border: ;
+}
+</style>
 
 <script>
 export default {
