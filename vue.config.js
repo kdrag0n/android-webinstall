4223ec035a855ee03534a31efd15657870f45b52
Danny Lin <danny@kdrag0n.dev>
2021-02-04 17:44:47 -0800

    Update page title

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
+const OS_NAME = "ProtonAOSP";
+
 module.exports = {
     transpileDependencies: ["vuetify"],
+    chainWebpack: (config) => {
+        config.plugin("html").tap((args) => {
+            args[0].title = `${OS_NAME} web installer`;
+            return args;
+        });
+    },
 };
