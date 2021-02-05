3bf940811f21fcdee5bae9bc4168ce467a3e915a
Danny Lin <danny@kdrag0n.dev>
2021-02-04 19:04:15 -0800

    Add tags for production deployment

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,18 @@
 const OS_NAME = "ProtonAOSP";
+const OS_DESC =
+    "a minimal custom Android ROM focused on UI/UX and performance with a touch of privacy";
 const SA_DOMAIN = "api.protonaosp.kdrag0n.dev";
+const PROD_URL = "https://protonaosp.kdrag0n.dev/install/web/";
 
 module.exports = {
+    publicPath: process.env.NODE_ENV === "production" ? PROD_URL : "/",
     transpileDependencies: ["vuetify"],
     chainWebpack: (config) => {
         config.plugin("html").tap((args) => {
             args[0].title = `${OS_NAME} web installer`;
             args[0].analyticsDomain = SA_DOMAIN;
+            args[0].osName = OS_NAME;
+            args[0].osDesc = OS_DESC;
             return args;
         });
     },
