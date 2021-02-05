4538b61bce875c73bb47cf05e89997cd80bc1548
Danny Lin <danny@kdrag0n.dev>
2021-02-04 18:53:02 -0800

    Add Simple Analytics embed

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,12 @@
 const OS_NAME = "ProtonAOSP";
+const SA_DOMAIN = "api.protonaosp.kdrag0n.dev";
 
 module.exports = {
     transpileDependencies: ["vuetify"],
     chainWebpack: (config) => {
         config.plugin("html").tap((args) => {
             args[0].title = `${OS_NAME} web installer`;
+            args[0].analyticsDomain = SA_DOMAIN;
             return args;
         });
     },
