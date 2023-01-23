0ca7013c48cddf583118a0d9086da300183640cc
zim <webmaster@coolguy.website>
2023-01-24 09:45:18 +1300

    add github workflow for serving app

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     publicPath:
         process.env.NODE_ENV === "production"
-            ? process.env.VUE_APP_PROD_URL
+            ? "/ii/android-webinstall"
             : "/",
     transpileDependencies: ["vuetify"],
     chainWebpack: (config) => {
