50ba0618fe49bda9b0acbd5a7c59a04489a87ae3
zim <webmaster@coolguy.website>
2023-01-24 09:37:52 +1300

    modify configs to only show pixel6a

--- src/config.js
+++ src/config.js
@@ -2,30 +2,34 @@ export default {
     OS_NAME: process.env.VUE_APP_OS_NAME,
     ACCENT_COLOR: "#0366d6",
     SUPPORTED_DEVICES: [
+        // {
+        //     name: "Pixel 4",
+        //     model: "flame",
+        // },
+        // {
+        //     name: "Pixel 4 XL",
+        //     model: "coral",
+        // },
+        // {
+        //     name: "Pixel 4a 5G",
+        //     model: "bramble",
+        // },
+        // {
+        //     name: "Pixel 5",
+        //     model: "redfin",
+        // },
+        // {
+        //     name: "Pixel 6",
+        //     model: "oriole",
+        // },
+        // {
+        //     name: "Pixel 6 Pro",
+        //     model: "raven",
+        // },
         {
-            name: "Pixel 4",
-            model: "flame",
-        },
-        {
-            name: "Pixel 4 XL",
-            model: "coral",
-        },
-        {
-            name: "Pixel 4a 5G",
-            model: "bramble",
-        },
-        {
-            name: "Pixel 5",
-            model: "redfin",
-        },
-        {
-            name: "Pixel 6",
-            model: "oriole",
-        },
-        {
-            name: "Pixel 6 Pro",
-            model: "raven",
-        },
+            name: "Pixel 6a",
+            model: "bluejay",
+        }
     ],
     RELEASE_VARIANTS: {
         minimal: {
